4fe2b872e7c0db7bdcc36e66d002da629a2a8d5a
bandara <chathubandara500@gmail.com>
2024-02-26 17:35:03 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module lk.ijse.bookworm_thelibrarymanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens lk.ijse.bookworm_thelibrarymanagementsystem to javafx.fxml;
+    exports lk.ijse.bookworm_thelibrarymanagementsystem;
+}
