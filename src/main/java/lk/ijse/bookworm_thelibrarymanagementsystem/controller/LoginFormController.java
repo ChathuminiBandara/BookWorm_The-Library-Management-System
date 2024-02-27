bc435275a2f6435dc99d2e3ab5509d9089fb4a87
bandara <chathubandara500@gmail.com>
2024-02-28 01:35:09 +0530

    login on action clearing part

--- src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/controller/LoginFormController.java
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/controller/LoginFormController.java
@@ -3,14 +3,23 @@ package lk.ijse.bookworm_thelibrarymanagementsystem.controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginFormController {
 
     public JFXButton btn_login;
     public JFXButton btn_signup;
+    public TextField password_Text;
+    public TextField name_Text;
 
+
+    public void login_on_action(ActionEvent actionEvent) {
+        password_Text.clear();
+        name_Text.clear();
+
+        /*call the suitable dashboard*/
+
+    }
 
     public void name_on_action(ActionEvent actionEvent) {
     }
@@ -18,9 +27,7 @@ public class LoginFormController {
     public void password_on_action(ActionEvent actionEvent) {
     }
 
-    public void login_on_action(ActionEvent actionEvent) {
-    }
-
     public void sign_up_on_action(ActionEvent actionEvent) {
     }
+
 }
