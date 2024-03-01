99808cd65f389d76c52d4fa4be56b0c413a296bd
bandara <chathubandara500@gmail.com>
2024-03-01 12:51:52 +0530

    Navigation between the library loan and the dashboard

--- src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/HelloApplication.java
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/HelloApplication.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/FXML_Files/hello-view.fxml"));
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/FXML_Files/hello-view.fxml"));*/
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/FXML_Files/Admin_Panel.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/controller/Admin_Controller.java
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/controller/Admin_Controller.java
@@ -2,8 +2,12 @@ package lk.ijse.bookworm_thelibrarymanagementsystem.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -21,7 +25,18 @@ public class Admin_Controller {
     }
 
     public void DashBoard_On_Action(ActionEvent actionEvent) throws IOException {
-
+        try {
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML_Files/Admin_Panel.fxml"));
+            AnchorPane anchorPane = loader.load();
+            Stage newStage = new Stage();
+            newStage.setTitle("Admin Panel");
+            newStage.setScene(new Scene(anchorPane));
+            currentStage.close();
+            newStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void Library_Loan_On_action(ActionEvent actionEvent) throws IOException {
