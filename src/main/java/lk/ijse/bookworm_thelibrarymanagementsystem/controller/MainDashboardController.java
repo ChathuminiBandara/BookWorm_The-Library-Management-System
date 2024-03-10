f3eb23fc49068fe6741aff823bc78608299537f5
bandara <chathubandara500@gmail.com>
2024-03-11 01:13:35 +0530

    Main things

--- src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/controller/MainDashboardController.java
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/controller/MainDashboardController.java
@@ -11,8 +11,8 @@ import java.net.URL;
 public class MainDashboardController {
     public AnchorPane Dynamic_Anchorpane;
 
-    public void initialize() throws IOException {
-        setform("/FXML_Files/Main_Dashboard.fxml");
+   public void initialize() throws IOException {
+        setform("/FXML_Files/Admin_Panel.fxml");
     }
     private void setform(String form) throws IOException {
         URL resource = this.getClass().getResource(form);
@@ -28,20 +28,26 @@ public class MainDashboardController {
     }
 
     public void Library_Loan_On_action(ActionEvent actionEvent) throws IOException {
-        setform("/FXML_Files/Library_loans");
+        setform("/FXML_Files/Library_Loans.fxml");
     }
 
     public void Books_On_action(ActionEvent actionEvent) throws IOException {
         setform("/FXML_Files/Books.fxml");
     }
 
-    public void Help_On_action(ActionEvent actionEvent) {
-
+    public void Help_On_action(ActionEvent actionEvent) throws IOException {
+        setform("/FXML_Files/Help.fxml");
     }
 
-    public void Settings_on_action(ActionEvent actionEvent) {
+    public void Settings_on_action(ActionEvent actionEvent) throws IOException {
+       setform("/FXML_Files/settings.fxml");
     }
 
-    public void Members_On_action(ActionEvent actionEvent) {
+    public void Members_On_action(ActionEvent actionEvent) throws IOException {
+       setform("/FXML_Files/members.fxml");
+    }
+
+    public void Branches_On_action(ActionEvent actionEvent) throws IOException {
+       setform("/FXML_Files/Library_Branches.fxml");
     }
 }
