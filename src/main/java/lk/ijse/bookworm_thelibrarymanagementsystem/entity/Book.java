0191b489a5d16604d77ca5f3f26b523160adb17e
bandara <chathubandara500@gmail.com>
2024-03-12 00:34:44 +0530

    book added

---
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/entity/Book.java
@@ -0,0 +1,28 @@
+package lk.ijse.bookworm_thelibrarymanagementsystem.entity;
+
+import org.hibernate.annotations.*;
+
+@Entity
+/*@Table(name = "books")*/
+public class Book {
+
+/*
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private int publishedYear;
+
+    @Column(nullable = false)
+    private boolean available;
+*/
+
+    // getters and setters
+}
