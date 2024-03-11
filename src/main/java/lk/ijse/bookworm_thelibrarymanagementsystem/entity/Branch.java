bf96f9ecf4eefee1f1d97c28f8868b1bcb60f325
bandara <chathubandara500@gmail.com>
2024-03-12 00:36:01 +0530

    Library Branch

---
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/entity/Branch.java
@@ -0,0 +1,20 @@
+package lk.ijse.bookworm_thelibrarymanagementsystem.entity;
+
+import org.hibernate.annotations.*;
+
+@Entity
+/*@Table(name = "branches")*/
+public class Branch {
+
+/*    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)*/
+    private String address;
+
+    // getters and setters
+}
