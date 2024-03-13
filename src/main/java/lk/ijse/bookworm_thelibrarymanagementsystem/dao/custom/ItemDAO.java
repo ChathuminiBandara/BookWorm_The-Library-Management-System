303cdc55e4491442f3743881a69cd8d04918e444
bandara <chathubandara500@gmail.com>
2024-03-13 21:27:30 +0530

    BOOK  Stuff done

---
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/dao/custom/ItemDAO.java
@@ -0,0 +1,8 @@
+package lk.ijse.bookworm_thelibrarymanagementsystem.dao.custom;
+
+import lk.ijse.layeredarchitecture.dao.CrudDAO;
+import lk.ijse.layeredarchitecture.entity.Item;
+
+public interface ItemDAO extends CrudDAO<Item> {
+
+}
