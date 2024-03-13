08592708fa535fdffbd302e97a40748b894cb964
bandara <chathubandara500@gmail.com>
2024-03-13 21:28:57 +0530

    MANAGE ... STUFF ADDED

---
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/dao/custom/OrderDetailsDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.bookworm_thelibrarymanagementsystem.dao.custom;
+
+import lk.ijse.layeredarchitecture.dao.CrudDAO;
+import lk.ijse.layeredarchitecture.entity.OrderDetail;
+
+public interface OrderDetailsDAO extends CrudDAO<OrderDetail> {
+}
