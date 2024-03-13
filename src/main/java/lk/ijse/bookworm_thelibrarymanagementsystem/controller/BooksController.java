ba7e1296abcfb4a11e85316de1aba7ecd31c64f2
bandara <chathubandara500@gmail.com>
2024-03-13 19:32:44 +0530

    Books Grid

--- src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/controller/BooksController.java
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/controller/BooksController.java
@@ -1,4 +1,51 @@
 package lk.ijse.bookworm_thelibrarymanagementsystem.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class BooksController {
+    public void add_new_book_on_action(ActionEvent actionEvent) {
+    }
+
+    public void book1_show(MouseEvent mouseEvent) {
+    }
+
+    public void book2_show(MouseEvent mouseEvent) {
+    }
+
+    public void book3_show(MouseEvent mouseEvent) {
+    }
+
+    public void book4_show(MouseEvent mouseEvent) {
+    }
+
+    public void book5_show(MouseEvent mouseEvent) {
+    }
+
+    public void book6_show(MouseEvent mouseEvent) {
+    }
+
+    public void book7_show(MouseEvent mouseEvent) {
+    }
+
+    public void book8_show(MouseEvent mouseEvent) {
+    }
+
+    public void book9_show(MouseEvent mouseEvent) {
+    }
+
+    public void book10_show(MouseEvent mouseEvent) {
+    }
+
+    public void book11_show(MouseEvent mouseEvent) {
+    }
+
+    public void book12_show(MouseEvent mouseEvent) {
+    }
+
+    public void book13_show(MouseEvent mouseEvent) {
+    }
+
+    public void book14_show(MouseEvent mouseEvent) {
+    }
 }
