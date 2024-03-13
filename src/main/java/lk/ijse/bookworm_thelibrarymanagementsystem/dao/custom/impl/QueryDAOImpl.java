35a74287015524a2ae91c7327789cb195fe5ed42
bandara <chathubandara500@gmail.com>
2024-03-13 21:29:43 +0530

    BOOK EXPLAIN STUFF ADDED

---
+++ src/main/java/lk/ijse/bookworm_thelibrarymanagementsystem/dao/custom/impl/QueryDAOImpl.java
@@ -0,0 +1,12 @@
+package lk.ijse.bookworm_thelibrarymanagementsystem.dao.custom.impl;
+
+import lk.ijse.layeredarchitecture.dao.custom.QueryDAO;
+import lk.ijse.layeredarchitecture.dto.CustomDTO;
+
+public class QueryDAOImpl implements QueryDAO {
+    @Override
+    public void customerOrderDetails(CustomDTO customDTO) {
+        //join query
+
+    }
+}
