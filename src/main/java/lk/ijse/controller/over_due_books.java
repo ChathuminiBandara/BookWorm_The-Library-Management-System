7475fc75b695dd7d8c84a7a424af3db14192395d
bandara <chathubandara500@gmail.com>
2024-03-15 03:04:08 +0530

    over due controller done

---
+++ src/main/java/lk/ijse/controller/over_due_books.java
@@ -0,0 +1,18 @@
+package lk.ijse.controller;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class over_due_books {
+    public TableView over_due_date;
+    public TableColumn name_book;
+    public TableColumn user_name;
+    public TableColumn due_date;
+    public TableColumn date_borrowed;
+
+
+
+
+
+
+}
